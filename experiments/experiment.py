6c05485bd4291e9c55b1d95bc638bcd37821519f
HHansi <h.hansi1991@gmail.com>
2020-03-13 12:44:25 +0000

    010: Updated experiments

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -1,85 +1,132 @@
-# Created by Hansi at 2/9/2020
+# Created by Hansi at 2/23/2020
+
+# Experiment updated based on the baseline code provided
+
 import csv
-import os
 
-from algo.embeddings import get_bert
+import numpy as np
+from flair.embeddings import BertEmbeddings
+
+from algo.embedding_util import get_word_embeddings
+from algo.flair_embeddings import get_embeddings_flair
 from algo.similarity_measures import get_cosine_similarity
-
-from data_processing.data_util import load_structured_data
+from data_processing.data_util import create_folder_if_not_exist
+from data_processing.preprocessor import preprocessing_flow1
 from evaluation.evaluation1 import evaluate1
 from evaluation.evaluation2 import evaluate2
-from experiments.pca_embedding import get_pca_word_vectors
-from experiments.raw_embedding import get_word_vectors
+from project_config import result_folder, data_folder
+
+languages = ['en']
+# languages = ('en', 'fi', 'hr', 'sl')
 
 
-# context1_vectors - [word1_vec, word2_vec]
-def get_similarity_measures(context1_vectors, context2_vectors):
-    sim_context1 = get_cosine_similarity(context1_vectors[0], context1_vectors[1])
-    sim_context2 = get_cosine_similarity(context2_vectors[0], context2_vectors[1])
+# get average of given embeddings
+def get_embedding_average(emb1, emb2, chuck_size):
+    emb1_splits = np.split(emb1, chuck_size)
+    emb2_splits = np.split(emb2, chuck_size)
+    emb1_sum = np.zeros(shape=len(emb1_splits[0]))
+    for emb in emb1_splits:
+        emb1_sum = emb1_sum + emb
+    emb2_sum = np.zeros(shape=len(emb2_splits[0]))
+    for emb in emb2_splits:
+        emb2_sum = emb2_sum + emb
+    emb1_avg = emb1_sum / chuck_size
+    emb2_avg = emb2_sum / chuck_size
+    return emb1_avg, emb2_avg
 
-    diff = sim_context2 - sim_context1
-    return sim_context1, sim_context2, diff
 
+def generate_results(input_folder_path, output_folder_path, dict_language_model=None, chunk_size=None):
+    for lan in languages:
+        rows = []
+        similarities = []
 
-def generate_results(languages, dict_lang_model, input_folder, output_folder):
-    output_folder_subtask1 = output_folder + '/subtask1'
-    output_folder_subtask2 = output_folder + '/subtask2'
+        print(f'\nLANGUAGE: {lan.upper()}\n')
 
-    if not os.path.exists(output_folder_subtask1):
-        os.makedirs(output_folder_subtask1)
+        with open(input_folder_path + f'/data_{lan}.tsv', 'r', encoding='utf-8') as csvfile:
+            csvreader = csv.DictReader(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE)
+            for index, row in enumerate(csvreader):
+                print(f"{index} {row['word1']}-{row['word2']}")
 
-    if not os.path.exists(output_folder_subtask2):
-        os.makedirs(output_folder_subtask2)
+                context1 = row['context1']
+                context2 = row['context2']
+                # preprocess text
+                context1 = preprocessing_flow1(context1)
+                context2 = preprocessing_flow1(context2)
 
-    for lang in languages:
-        input_filepath = input_folder + os.path.sep + 'data_' + lang + '.tsv'
-        data = load_structured_data(input_filepath, lang, preprocess=True)
+                # context1 calculation
+                # embeddings, tokens = get_embeddings_baseline(context1)
+                embeddings, tokens = get_embeddings_flair(context1, dict_language_model[lan])
+                word1_embedding, word2_embedding = get_word_embeddings(tokens, embeddings, row['word1_context1'],
+                                                                       row['word2_context1'])
+                if chunk_size:
+                    word1_embedding, word2_embedding = get_embedding_average(word1_embedding, word2_embedding,
+                                                                             chunk_size)
+                    print('average embeddings context1')
+                sim_context1 = get_cosine_similarity(word1_embedding, word2_embedding)
 
-        # change vector generation for different experiments
-        # word_vectors = get_word_vectors(data, dict_lang_model[lang])
-        word_vectors = get_pca_word_vectors(data, dict_lang_model[lang], 50)
+                # context2 calculation
+                # embeddings, tokens = get_embeddings_baseline(context2)
+                embeddings, tokens = get_embeddings_flair(context2, dict_language_model[lan])
+                word1_embedding, word2_embedding = get_word_embeddings(tokens, embeddings, row['word1_context2'],
+                                                                       row['word2_context2'])
+                if chunk_size:
+                    word1_embedding, word2_embedding = get_embedding_average(word1_embedding, word2_embedding,
+                                                                             chunk_size)
+                    print('average embeddings context2')
+                sim_context2 = get_cosine_similarity(word1_embedding, word2_embedding)
 
-        task1_output_filepath = output_folder_subtask1 + os.path.sep + 'results_subtask1_' + lang + '.tsv'
-        task2_output_filepath = output_folder_subtask2 + os.path.sep + 'results_subtask2_' + lang + '.tsv'
+                similarities.append([sim_context1, sim_context2])
 
-        csv_file_out_task1 = open(task1_output_filepath, 'a', newline='', encoding='utf-8')
-        csv_writer_task1 = csv.writer(csv_file_out_task1, delimiter='\t')
-        csv_writer_task1.writerow(['change'])
+        columns = ['change']
+        with open(output_folder_path + f'/res1/results_subtask1_{lan}.tsv', 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE, quotechar='', escapechar='~')
+            csvwriter.writerow(columns)
+            for sim in similarities:
+                change = sim[1] - sim[0]
+                csvwriter.writerow([change])
 
-        csv_file_out_task2 = open(task2_output_filepath, 'a', newline='', encoding='utf-8')
-        csv_writer_task2 = csv.writer(csv_file_out_task2, delimiter='\t')
-        csv_writer_task2.writerow(['sim_context1', 'sim_context2'])
-
-        for row in word_vectors:
-            context1_vectors = row[0]
-            context2_vectors = row[1]
-
-            sim_context1, sim_context2, diff = get_similarity_measures(context1_vectors, context2_vectors)
-            csv_writer_task2.writerow([sim_context1, sim_context2])
-
-            diff = sim_context2 - sim_context1
-            csv_writer_task1.writerow([diff])
-
-        csv_file_out_task1.close()
-        csv_file_out_task2.close()
+        columns = ['sim_context1', 'sim_context2']
+        with open(output_folder_path + f'/res2/results_subtask2_{lan}.tsv', 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile, delimiter='\t', quoting=csv.QUOTE_NONE, quotechar='', escapechar='~')
+            csvwriter.writerow(columns)
+            for sim in similarities:
+                csvwriter.writerow(sim)
 
 
 if __name__ == '__main__':
-    languages = ('en', 'hr', 'sl')
+    input_folder = '../data/evaluation_kit_final/diff-sm/ignored-ents-V2/'
+    output_folder = result_folder + 'final-results/submission6/bert-large-cased-layers-1-14-avg_sm-ignored-entsv2/'
+
+    # input_folder = data_folder + 'trial_data_en_hr_sl/trial_data_task3/data/'
+    # input_folder = data_folder + 'practice_kit_final/diff-sm/ignored-ents-V2/'
+    # output_folder = result_folder + 'practice-kit-final/English3/bert-large-cased-layers-1-14-avg_sm-ignored-entsv2/'
+
+    create_folder_if_not_exist(output_folder)
+    res1_folder = output_folder + 'res1/'
+    create_folder_if_not_exist(res1_folder)
+    res2_folder = output_folder + 'res2/'
+    create_folder_if_not_exist(res2_folder)
+
     lang_emb = dict()
-    lang_emb['en'] = get_bert('bert-large-cased')
-    bert_multilingual = get_bert('bert-base-multilingual-cased')
-    lang_emb['hr'] = bert_multilingual
-    lang_emb['sl'] = bert_multilingual
+    model = BertEmbeddings('bert-large-cased', layers='-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12,-13,-14')
+    # model = BertEmbeddings('bert-base-finnish-uncased-v1', pooling_operation='first_last', use_scalar_mix=True)
 
-    input_folder = '../data/trial_data_en_hr_sl/trial_data_task3/data'
-    output_folder = '../results/bert_embedding_large_cased_en_multilingual_cased-pca'
+    # model2 = get_flair('sl-backward')
+    # models = [model1, model2]
+    # model = get_stacked_embeddings(models)
 
-    generate_results(languages, lang_emb, input_folder, output_folder)
+    lang_emb['en'] = model
+    # lang_emb['hr'] = model
+    # lang_emb['sl'] = model
+    # lang_emb['fi'] = model
+    generate_results(input_folder, output_folder, lang_emb, chunk_size=14)
 
-    gold_folder = '../data/trial_data_en_hr_sl/trial_data_task3/gold'
+    # evaluate results
+    output_folder = result_folder + 'practice-kit-final/English3/bert-large-cased-layers-1-14-avg_sm-ignored-entsv2/'
+    # gold_folder = data_folder + 'trial_data_en_hr_sl/trial_data_task3/gold'
+    gold_folder = data_folder + 'practice_kit_final/gold'
     print('Sub task 1')
-    evaluate1(gold_folder, output_folder + '/subtask1')
+    evaluate1(gold_folder, output_folder + '/res1/')
 
     print('Sub task 2')
-    evaluate2(gold_folder, output_folder + '/subtask2')
+    evaluate2(gold_folder, output_folder + '/res2/')
